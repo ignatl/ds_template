746bdcb88b1fb42704c9047be84ddebf1892a3d4
ignat_levagin <ignatlevagin@gmail.com>
2025-02-10 22:31:52 +0100

    7 linters (#8)
    * Fix github templates
    
    * WIP: cookiecutter initialisation
    
    * Add uv installation
    
    * WIP: cookiecutter debugging
    
    * WIP: cookiecutter debugging
    
    * WIP: cookiecutter debugging
    
    * WIP: cookiecutter debugging
    
    * Fix main_dir
    
    * Get rid of cookiecutter
    
    * Install poetry and initialise pyproject.toml
    
    * Init pre-commit
    
    * Fix ruff
    
    * Fix pull request template
    
    * Add github actions
    
    * Configure self-hosted runner
    
    * Add mypy
    
    * Add mypy to pre-commit
    
    * Update ci to run in docker
    
    * Add action.yml
    
    * Remove additional linters installation
    
    * Add pytest to deps
    
    * Move pytest to dev dependencies
    
    * Test actions
    
    * Test actions
    
    * Test actions
    
    * Test actions
    
    * Test actions
    
    * Test actions
    
    * Test actions
    
    * Test actions
    
    * Test actions
    
    * Test actions
    
    * Test actions
    
    * Test actions
    
    * Test actions
    
    * Test actions
    
    * Test actions
    
    * Test actions
    
    * Add Dockerfile
    
    * Test actions
    
    * Fix Dockerfile
    
    * Test actions
    
    * Test actions
    
    * Add initial test
    
    * Fix Dockerfile
    
    * Fix Dockerfile

--- ds-template/config.py
+++
@@ -1,4 +0,0 @@
-from pathlib import Path
-
-main_dir = Path(__file__).parent
-data_dir = main_dir / "data"
---
+++ ds_template/config.py
@@ -0,0 +1,5 @@
+"""Configuration management module.
+
+This module handles configuration settings and environment variables
+for the data science project.
+"""
