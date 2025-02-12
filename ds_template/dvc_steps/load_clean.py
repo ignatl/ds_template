d388d0d334bc0d3b7fcff8b82b40e5c59f4297de
Ignat Levagin <ignatlevagin@gmail.com>
2025-02-12 21:03:50 +0000

    Add first version of feature; Add dvc hooks

--- ds_template/dvc_steps/load_clean.py
+++ ds_template/dvc_steps/load_clean.py
@@ -9,14 +9,21 @@ import polars as pl
 from ds_template.config import MAIN_DIR, logger
 
 
-def load_and_clean(input_path: str, output_path: str) -> None:
+def load_clean(df: pl.DataFrame) -> pl.DataFrame:
     """Load and clean data."""
-    df = pl.read_csv(MAIN_DIR / input_path)
+    return df
+
+
+def main(input_path: str, output_path: str) -> None:
+    """Load and clean data."""
+    input_df = pl.scan_csv(MAIN_DIR / input_path)
     logger.info(f"Loaded data from {input_path} and cleaned it (no)")
+
+    output_df = load_clean(input_df)
 
     full_output_path = MAIN_DIR / output_path
     full_output_path.parent.mkdir(parents=True, exist_ok=True)
-    df.write_parquet(full_output_path)
+    output_df.collect().write_parquet(full_output_path)
     logger.info(f"Saved data to {output_path}")
 
 
@@ -27,7 +34,7 @@ def cli() -> None:
     parser.add_argument("--output_path", type=str, required=True)
     args = parser.parse_args()
 
-    load_and_clean(args.input_path, args.output_path)
+    main(args.input_path, args.output_path)
 
 
 if __name__ == "__main__":
